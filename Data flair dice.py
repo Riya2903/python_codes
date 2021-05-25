6f53f27a216789986dba4e19d76e4cffee1f35e7
Riya2903 <58070071+Riya2903@users.noreply.github.com>
2021-05-25 14:25:23 +0530

    Add files via upload

---
+++ Data flair dice.py
@@ -0,0 +1,21 @@
+from tkinter import*
+import random
+
+# toplevel widget which represents the main window of an application
+root = Tk()
+root.title("Dice simulator")
+
+root.geometry('600x500')
+
+label = Label(root,font = ("times", 200 , 'bold'), text ="",fg = 'red')
+
+def rolldice():
+  dice = ['\u2680','\u2681','\u2682','\u2683','\u2684','\u2685']
+  label.config(text = f'{random.choice(dice)}')
+  label.pack()
+  
+button = Button(root, font = ("Helviticia", 25 , 'bold'), text = "roll the dice", command = rolldice, bg = 'blue')
+button.place(x=300,y=0)
+button.pack()
+
+root.mainloop()
